a43c80d540c06ac1102ccb20f868626ef7c30439
Ben Batt <benbatt@gmail.com>
2023-11-01 22:18:22 +1100

    Update code style

--- src/drawingarea.cpp
+++ src/drawingarea.cpp
@@ -48,12 +48,9 @@ void drawHandle(const Cairo::RefPtr<Cairo::Context>& context, double size, const
   context->set_source_rgb(0, 0, 0);
   context->stroke_preserve();
 
-  if (hover)
-  {
+  if (hover) {
     context->set_source_rgb(1, 1, 1);
-  }
-  else
-  {
+  } else {
     context->set_source_rgb(0.5, 0.5, 0.5);
   }
 
@@ -62,12 +59,10 @@ void drawHandle(const Cairo::RefPtr<Cairo::Context>& context, double size, const
 
 void DrawingArea::onDraw(const Cairo::RefPtr<Cairo::Context>& context, int width, int height)
 {
-  if (mNodes.size() > 1)
-  {
+  if (mNodes.size() > 1) {
     context->move_to(mNodes[0].position.x, mNodes[0].position.y);
 
-    for (int i = 1; i < mNodes.size(); ++i)
-    {
+    for (int i = 1; i < mNodes.size(); ++i) {
       const Point& position = mNodes[i].position;
       const Point control1 = mNodes[i - 1].position + mNodes[i - 1].controlB;
       const Point control2 = position + mNodes[i].controlA;
@@ -79,16 +74,14 @@ void DrawingArea::onDraw(const Cairo::RefPtr<Cairo::Context>& context, int width
     context->stroke();
   }
 
-  for (int i = 0; i < mHandles.size(); ++i)
-  {
+  for (int i = 0; i < mHandles.size(); ++i) {
     drawHandle(context, HandleSize, handlePosition(mHandles[i]), i == mHoverIndex);
   }
 }
 
 void DrawingArea::onPressed(int count, double x, double y)
 {
-  if (count == 2)
-  {
+  if (count == 2) {
     addNode({ x, y }, { -5, 0 }, { 5, 0 });
     queue_draw();
   }
@@ -98,8 +91,7 @@ void DrawingArea::onPointerMotion(double x, double y)
 {
   int newHoverIndex = findHandle(x, y);
 
-  if (newHoverIndex != mHoverIndex)
-  {
+  if (newHoverIndex != mHoverIndex) {
     mHoverIndex = newHoverIndex;
     queue_draw();
   }
@@ -109,16 +101,14 @@ void DrawingArea::onDragBegin(double x, double y)
 {
   mDragIndex = findHandle(x, y);
 
-  if (mDragIndex >= 0)
-  {
+  if (mDragIndex >= 0) {
     mDragStart = handlePosition(mHandles[mDragIndex]);
   }
 }
 
 void DrawingArea::onDragUpdate(double xOffset, double yOffset)
 {
-  if (mDragIndex >= 0)
-  {
+  if (mDragIndex >= 0) {
     setHandlePosition(mHandles[mDragIndex], mDragStart + Vector{ xOffset, yOffset });
     queue_draw();
   }
@@ -126,8 +116,7 @@ void DrawingArea::onDragUpdate(double xOffset, double yOffset)
 
 void DrawingArea::onDragEnd(double xOffset, double yOffset)
 {
-  if (mDragIndex >= 0)
-  {
+  if (mDragIndex >= 0) {
     setHandlePosition(mHandles[mDragIndex], mDragStart + Vector{ xOffset, yOffset });
     queue_draw();
   }
@@ -148,13 +137,11 @@ int DrawingArea::findHandle(double x, double y)
 {
   float halfSize = HandleSize / 2;
 
-  for (int i = 0; i < mHandles.size(); ++i)
-  {
+  for (int i = 0; i < mHandles.size(); ++i) {
     const Point position = handlePosition(mHandles[i]);
 
     if (position.x - halfSize <= x && x < position.x + halfSize
-      && position.y - halfSize <= y && y < position.y + halfSize)
-    {
+      && position.y - halfSize <= y && y < position.y + halfSize) {
       return i;
     }
   }
@@ -166,8 +153,7 @@ Point DrawingArea::handlePosition(const Handle& handle) const
 {
   const Node& node = mNodes[handle.mNodeIndex];
 
-  switch (handle.mType)
-  {
+  switch (handle.mType) {
     case Handle::Position:
       return node.position;
       break;
@@ -186,8 +172,7 @@ void DrawingArea::setHandlePosition(const Handle& handle, const Point& position)
 {
   Node& node = mNodes[handle.mNodeIndex];
 
-  switch (handle.mType)
-  {
+  switch (handle.mType) {
     case Handle::Position:
       node.position = position;
       break;
